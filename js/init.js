177c843885312060e6d4246eb46694a9d4986b90
Kacpere <Kacpi@int.pl>
2018-05-29 18:27:09 +0200

    wrzucenie plikow na serwer

---
+++ js/init.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+   $('.sidenav').sidenav();
+ });
+
+$(".dropdown-trigger").dropdown();
+
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
